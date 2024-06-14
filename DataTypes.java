1afeeedd64a6b420dc0a7292ec10c498871f3250
choudhary1ashish <159534090+choudhary1ashish@users.noreply.github.com>
2024-06-15 00:34:09 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+package JavaBasics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int i = 600;
+		int z = 900;
+		int k = 700;
+		
+		if (i>z & i>k) {
+			System.out.println("i is the greatest");
+		}
+		else if (z>k) {
+			System.out.println("z is the greatest");
+		}
+		else {
+			System.out.println("k is the greatest");
+		}
+
+	}
+
+}
